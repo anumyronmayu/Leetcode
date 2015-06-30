151367cd39220f8335f0635365f4afb4187193ed
Yu Ma <myronmayu@gmail.com>
2015-06-30 13:18:10 +1000

    Test

--- src/defaultPackage/Problem49.java
+++ src/defaultPackage/Problem49.java
@@ -2,13 +2,17 @@ package defaultPackage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Problem49 {
 
 	public static List<String> anagrams(String[] strs) {
 
 		List<HashMap<Character, Integer>> list = new ArrayList<HashMap<Character, Integer>>();
+
+		HashMap<HashMap<Character, Integer>, List<Integer>> indexMap = new HashMap<HashMap<Character, Integer>, List<Integer>>();
 
 		for (int i = 0; i < strs.length; i++) {
 
@@ -17,32 +21,55 @@ public class Problem49 {
 			for (int j = 0; j < strs[i].length(); j++) {
 
 				if (map.get(strs[i].charAt(j)) == null) {
+
 					map.put(strs[i].charAt(j), 1);
+
 				} else {
+
 					map.put(strs[i].charAt(j), map.get(strs[i].charAt(j)) + 1);
+
 				}
 
 			}
 
 			list.add(map);
 
-		}
+			if (indexMap.get(map) == null) {
 
-		List<List<HashMap<Character, Integer>>> listOfList = new ArrayList<List<HashMap<Character, Integer>>>();
+				List<Integer> indexList = new ArrayList<Integer>();
+				indexList.add(i);
+				indexMap.put(map, indexList);
 
-		for (HashMap<Character, Integer> map : list) {
+			} else {
 
-			for(List<HashMap<Character, Integer>> cluster : listOfList){
-				
-				if(cluster.get(0).equals(map)){
-					
-				}
-				
+				indexMap.get(map).add(i);
+
 			}
-			
+
 		}
 
-		return null;
+		System.out.println("List: " + list);
+		System.out.println("IndexMap: " + indexMap);
+
+		List<String> listOfString = new ArrayList<String>();
+
+		Iterator<Map.Entry<HashMap<Character, Integer>, List<Integer>>> iterator = indexMap
+				.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry<HashMap<Character, Integer>, List<Integer>> entry = iterator
+					.next();
+
+			if (entry.getValue().size() > 1) {
+
+				for (int i : entry.getValue()) {
+
+					listOfString.add(strs[i]);
+
+				}
+			}
+		}
+
+		return listOfString;
 
 	}
 
