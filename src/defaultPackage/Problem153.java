6659917d99f6a5030453c4ddd9e887039b4ec5a0
Yu Ma <myronmayu@gmail.com>
2015-07-16 22:14:22 +1000

    Update

--- src/defaultPackage/Problem153.java
+++ src/defaultPackage/Problem153.java
@@ -4,27 +4,78 @@ public class Problem153 {
 
 	public int findMin(int[] nums) {
 
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		if (nums[nums.length - 1] >= nums[0]) {
+			return nums[0];
+		}
+
 		int n = nums.length;
 		int low = 0;
 		int high = n - 1;
 		int mid;
 
 		while (low <= high) {
+
 			mid = (low + high) / 2;
 
-			if (low == high) {
+			if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
 				return nums[mid];
+			}
+
+			if (mid == low) {
+				return Math.min(nums[low], nums[high]);
+			} else if (nums[mid] > nums[low]) {
+				low = mid;
 			} else {
-				if (nums[mid] > nums[low]) {
-					low = mid;
-				} else {
-					high = mid;
-				}
+				high = mid;
 			}
 		}
 
 		return -1;
+	}
 
+	public int findMin2(int[] nums) {
+
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		if (nums[nums.length - 1] >= nums[0]) {
+			return nums[0];
+		}
+
+		int n = nums.length;
+		int low = 0;
+		int high = n - 1;
+
+		return binarySearch(nums, low, high);
+
+	}
+
+	public int binarySearch(int[] nums, int low, int high) {
+
+		if (low <= high) {
+
+			int mid = (low + high) / 2;
+
+			if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
+				return nums[mid];
+			}
+
+			if (mid == low) {
+				return Math.min(nums[low], nums[high]);
+			} else if (nums[mid] > nums[low]) {
+				return binarySearch(nums, mid, high);
+			} else {
+				return binarySearch(nums, low, mid);
+			}
+
+		} else {
+			return -1;
+		}
 	}
 
 	public static void main(String[] args) {
