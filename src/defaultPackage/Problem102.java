8372b87e7ee8128824fee1b7ec1ffa0e90be3585
Yu Ma <myronmayu@gmail.com>
2015-07-17 13:10:21 +1000

    T

---
+++ src/defaultPackage/Problem102.java
@@ -0,0 +1,62 @@
+package defaultPackage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Problem102 {
+
+	public HashMap<Integer, List<Integer>> map;
+	public int maxHeight;
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+		if (root == null) {
+			return results;
+		}
+
+		map = new HashMap<Integer, List<Integer>>();
+		maxHeight = 0;
+
+		traversal(root, 0);
+
+		for (int i = 1; i <= maxHeight; i++) {
+			results.add(map.get(i));
+		}
+
+		return results;
+
+	}
+
+	public void traversal(TreeNode root, int parentHeight) {
+
+		if (root != null) {
+
+			int height = parentHeight + 1;
+
+			if (height > maxHeight) {
+				maxHeight = height;
+			}
+
+			if (map.get(height) == null) {
+				List<Integer> list = new ArrayList<Integer>();
+				list.add(root.val);
+				map.put(height, list);
+			} else {
+				map.get(height).add(root.val);
+			}
+
+			traversal(root.left, height);
+			traversal(root.right, height);
+
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
